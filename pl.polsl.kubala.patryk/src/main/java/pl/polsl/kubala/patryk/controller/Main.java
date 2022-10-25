0dfef70fc4bfe3022cd288c2505a30f66f5b4251
Your Name <patrykkubala1999@gmail.com>
2022-10-25 12:53:55 +0200

    changes

---
+++ pl.polsl.kubala.patryk/src/main/java/pl/polsl/kubala/patryk/controller/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pl.polsl.kubala.patryk.controller;
+import pl.polsl.kubala.patryk.controller.Controller;
+
+/**
+ *
+ * @author patry
+ */
+public class Main {
+     public static void main(String... args) {
+        
+         Controller controller = new Controller(args);
+}}
